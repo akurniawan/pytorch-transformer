bb88f0a0a75d69622cabe9d6cb3f6dffcc8ca3b7
akurniawan <akurniawan@traveloka.com>
2019-05-31 13:26:41 +0700

    add dropout and relu to ffn

--- modules/ffn.py
+++ modules/ffn.py
@@ -2,26 +2,23 @@ import torch.nn as nn
 
 
 class PositionWiseFFN(nn.Module):
-    def __init__(self, feature_size, num_units=[2048, 512]):
+    def __init__(self, feature_size, num_units=2048, dropout=0.1):
         super(PositionWiseFFN, self).__init__()
-        self.ffn = self._build_ffn(feature_size, num_units)
-
-    def _build_ffn(self, feature_size, num_units):
-        layers = []
-        features = feature_size
-        for unit in num_units:
-            layers.append(nn.Linear(features, unit))
-            features = unit
-
-        return nn.Sequential(*layers)
+        self._dropout = dropout
+        self.ffn = nn.Sequential(
+            nn.Linear(feature_size, num_units), nn.Dropout(dropout), nn.ReLU(),
+            nn.Linear(num_units, feature_size))
+        self.ln = nn.LayerNorm(feature_size)
 
     def forward(self, X):
-        # assert if the feature size of inputs not the same as
-        # the last ffn layer, since we need both of them
-        # the same for residual network
-        assert X.size(-1) == self.ffn[-1].bias.size(-1)
         ffn = self.ffn(X)
         # residual network
         ffn += X
+        ffn = self.ln(ffn)
 
         return ffn
+
+    def init_weight(self):
+        for idx in range(len(self.ffn)):
+            if hasattr(self.ffn[idx], "weight"):
+                nn.init.uniform_(self.ffn[idx].weight, -0.1, 0.1)
